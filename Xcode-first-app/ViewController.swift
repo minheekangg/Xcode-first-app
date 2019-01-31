b9918b4de11f92b75c85a462c1153b4856366166
minheekangg <minheekangg@gmail.com>
2019-01-31 11:41:48 -0500

    Initial Commit

---
+++ Xcode-first-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode-first-app
+//
+//  Created by Minhee Kang on 1/31/19.
+//  Copyright © 2019 Minhee Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
